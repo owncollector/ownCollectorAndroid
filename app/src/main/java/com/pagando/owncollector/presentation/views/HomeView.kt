9e7b8ca7311f7a8a20f9c0d90a49ffe65abea941
Braian Avalos <braian@macbookpro.lan>
2024-11-30 22:46:38 -0600

    cambio de libreria

--- app/src/main/java/com/pagando/owncollector/presentation/views/HomeView.kt
+++ app/src/main/java/com/pagando/owncollector/presentation/views/HomeView.kt
@@ -27,7 +27,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
